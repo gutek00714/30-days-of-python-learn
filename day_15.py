55c2f96b5ef61eb48189ea8b08b89f75aa43cea6
gutek00714 <guciolaczkowski1@gmail.com>
2025-10-19 17:51:41 +0200

    Add day_15.py

---
+++ day_15.py
@@ -0,0 +1,19 @@
+#Syntax Error - python can't understand the code, because something basic is wrong with it - for example (). This error will suggest the solution.
+
+#NameError - a variable or function hasn't been defined yet.
+
+#IndexError - an element of the list, tuple or string doesn't exist.
+
+#ModuleNotFoundError - python can't find the module. Probably typing mistake
+
+#AttributeError - attribute does not exist. - PI instead of pi in math module
+
+#KeyError - key in dictionary doesn't exist.
+
+#TypeError - when you try to perform an operation on a value of the wrong type (add number to a string)
+
+#ImportError - when Python fails to import a module or a specific object from a module (similar to ModuleNotFoundError but here the module can be found, but a specific object cannot)
+
+#ValueError - a function receives an argument of the right type but an inappropriate value
+
+#ZeroDivisionError - cannot divide a number by zero
